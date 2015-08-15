c5c5d7b065dea6e360f32ec557f1024e16fc0a6e
fuvity <gabrielaffonso32@hotmail.com>
2015-08-15 19:23:33 -0300

    regras? im really not sure

--- src/br/unicamp/secomp/hackaton/automatawar/GameState.java
+++ src/br/unicamp/secomp/hackaton/automatawar/GameState.java
@@ -51,4 +51,44 @@ public class GameState {
 	{
 		
 	}
+	
+	public int getAmount(int elem, int src_x, int src_y) {
+		int count = 0;
+		
+		int y, x;
+		for(y=src_y-1; y<=src_y+1; y++) {
+			for(x=src_x-1; x<=src_x+1; x++) {
+				if(getElement(x, y) == elem)
+					count++;
+			}
+		}
+		return count - 1;
+	}
+	
+	public int getElement(in[][] table, int x, int y) {
+		if(x >= 0 && x < size_x)
+			if(y >= 0 && y < size_y)
+				return table[y][x];
+		return 0;
+	}
+	
+	public int rule(int[][] table, int x, int y)
+	{
+		amount = getAmount(table, 1, x, y, 1);
+		currstate = getElement(table, x, y);
+		if (currstate == 1)
+		{
+			if (amount == 2 || amount == 3) /* 2 and 3 survive */
+				return 1;
+			else /* less than 2 or more than 3 die */
+				return 0;
+		}
+		else if (currstate == 0)
+		{
+			if (amount == 3)
+				return 1;
+			else
+				return 0;
+		}
+	}
 }
