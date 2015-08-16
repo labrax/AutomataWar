b2acc8a0b3223cd3da2dbc7347f3ed75fc6d55bf
fuvity <gabrielaffonso32@hotmail.com>
2015-08-16 01:19:28 -0300

    sound script

--- src/br/unicamp/secomp/hackaton/automatawar/Sound.java
+++ src/br/unicamp/secomp/hackaton/automatawar/Sound.java
@@ -1,5 +1,15 @@
 package br.unicamp.secomp.hackaton.automatawar;
 
 public class Sound {
-
+	public void playSound() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\Users\Fuvity\Desktop\ddd\AutomataWar\sounds").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
 }
