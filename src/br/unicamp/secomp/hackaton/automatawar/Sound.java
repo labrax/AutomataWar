1655fcf02f17451f83cc5bb97b356800c69f0a53
fuvity <gabrielaffonso32@hotmail.com>
2015-08-16 02:04:17 -0300

    soundscripts ready

--- src/br/unicamp/secomp/hackaton/automatawar/Sound.java
+++ src/br/unicamp/secomp/hackaton/automatawar/Sound.java
@@ -1,9 +1,45 @@
 package br.unicamp.secomp.hackaton.automatawar;
 
 public class Sound {
-	public void playSound() {
+	public void playSoundAcabou() {
 	    try {
-	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\Users\Fuvity\Desktop\ddd\AutomataWar\sounds\acabou.wav").getAbsoluteFile());
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\acabou.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+	
+	public void playSoundPontos() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\glass_ping-Go445-1207030150").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+	
+	public void playSoundPlacement() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\Button Clicking-SoundBible").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+	
+	public void playSoundTimeLimit() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\BOMB_SIREN-BOMB_SIREN-247265934").getAbsoluteFile());
 	        Clip clip = AudioSystem.getClip();
 	        clip.open(audioInputStream);
 	        clip.start();
